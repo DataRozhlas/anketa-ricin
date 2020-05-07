d4b9e9345e8548de6a5d368d03eb894f6f0a2f84
datastory.cz <honza@datastory.cz>
2020-05-07 14:14:01 +0200

    alfaverze

---
+++ article.md
@@ -0,0 +1,13 @@
+title: "Kujme pikle"
+perex: "Špojóni špijóni."
+coverimg: https://i.ytimg.com/vi/fOqxZyAEetQ/hqdefault.jpg
+coverimg_note: "obrázek"
+styles: []
+libraries: [] #jquery, d3, highcharts, datatables
+options: [] #wide, noheader (, nopic)
+---
+Abc 123
+
+<wide>
+  <div id="anketa-wrapper"></div>
+</wide>
---
+++ js/script.js
@@ -0,0 +1,40 @@
+﻿import "./byeie"; // loučíme se s IE
+import { h, render } from "preact";
+/** @jsx h */
+
+let host = "https://data.irozhlas.cz/anketa-ricin";
+if (window.location.hostname === "localhost") {
+  host = "http://localhost/anketa-ricin";
+}
+
+const qu1 = 'Cožeto?'
+const qu2 = 'Fakt?'
+
+function printResps(obj) {
+  return `<b>${qu1}</b><p>${obj.o1}</p><b>${qu2}</b><p>${obj.o2}</p>`
+}
+
+function onLoad(e) {
+  const data = JSON.parse(e.target.response);
+  render((
+    <div id="anketa">
+      {data.map(el => (
+        <div className="respondent">
+          <img className="portret" src={host + "/foto/" + el.f} alt={el.p} />
+          <div className="bio">
+            <div className="jmeno">{`${el.j} ${el.p}`}</div>
+            <div className="vek">{el.s}</div>
+          </div>
+          <div className="odpoved" dangerouslySetInnerHTML={{ __html: printResps(el) }}></div>
+        </div>
+      ))}
+    </div>
+  ), document.getElementById("anketa-wrapper"));
+}
+
+const r = new XMLHttpRequest();
+r.addEventListener("load", onLoad);
+r.open("GET", host + "/data/data.json");
+r.send();
+
+makeChart();
